bf190763c30007f81a11682b3e5d00ad491aff94
supercycle91 <supercycle91@gmail.com>
2017-08-08 08:33:56 -0700

    first batch or algorithms

---
+++ main.py
@@ -0,0 +1,5 @@
+import algorthms
+from recommendations import critics
+  
+print algorthms.sim_distance(critics, 'Lisa Rose', 'Michael Phillips')
+print algorthms.sim_pearson(critics, 'Lisa Rose', 'Michael Phillips')
